6fe3197fdf445b00cacbfbea6a00a39430674c50
Gabriel Neme <80688833+GaboRex@users.noreply.github.com>
2023-06-08 18:25:24 -0400

    Add resumen-almuerzo y arreglos esteticos menores

--- lib/screens/app_screens/acompanhamientos_screen.dart
+++ lib/screens/app_screens/acompanhamientos_screen.dart
@@ -3,9 +3,14 @@ import '../../models/Almuerzo.dart';
 
 class AcompanhamientosScreen extends StatefulWidget {
   final Almuerzo almuerzo;
+  final List<String?> acompanhamientosSeleccionados;
+  final List<String?> ensaladasSeleccionadas;
+
   AcompanhamientosScreen({
     Key? key,
     required this.almuerzo,
+    required this.acompanhamientosSeleccionados,
+    required this.ensaladasSeleccionadas,
   }) : super(key: key);
 
   @override
@@ -13,31 +18,30 @@ class AcompanhamientosScreen extends StatefulWidget {
 }
 
 class _AcompanhamientosScreenState extends State<AcompanhamientosScreen> {
-  List<String> acompanhamientosDisponibles = [
-    'Acompanhamiento 1',
-    'Acompanhamiento 2',
-    'Acompanhamiento 3',
-    'Acompanhamiento 4',
-  ];
-  List<String> ensaladasDisponibles = [
-    'Ensalada 1',
-    'Ensalada 2',
-    'Ensalada 3',
-    'Ensalada 4',
-  ];
-  String? acompanhamientoSeleccionado;
-  String? ensaladaSeleccionada;
+  List<String?> acompanhamientos1Options = ['Opción 1', 'Opción 2', 'Opción 3'];
+  List<String?> acompanhamientos2Options = ['Opción A', 'Opción B', 'Opción C'];
+  List<String?> ensaladasOptions = ['Ensalada X', 'Ensalada Y', 'Ensalada Z'];
+  String? selectedAcompanhamiento1;
+  String? selectedAcompanhamiento2;
+  String? selectedEnsalada;
+  bool isParaLlevar = false;
 
   @override
   Widget build(BuildContext context) {
     Almuerzo almuerzo = widget.almuerzo;
-    List<String> acompanhamientosSeleccionado = List<String>.filled(almuerzo.acompanhamientos, '');
-    List<String> ensaladasSeleccionada = List<String>.filled(almuerzo.ensaladas, 'e');
+    List<String?> acompanhamientosSeleccionados = widget.acompanhamientosSeleccionados;
+    List<String?> ensaladasSeleccionadas = widget.ensaladasSeleccionadas;
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 107, 142, 35),
-        title: Text(almuerzo.nombre),
+        title: Text(
+          almuerzo.nombre,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       backgroundColor: Colors.lightGreen.shade200,
       body: Padding(
@@ -45,72 +49,129 @@ class _AcompanhamientosScreenState extends State<AcompanhamientosScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            for (int i = 0; i < almuerzo.acompanhamientos; i++)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Acompañamiento ${i + 1}:',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  DropdownButton<String>(
-                    value: acompanhamientoSeleccionado,
-                    hint: const Text('Seleccione un acompañamiento'),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        acompanhamientosSeleccionado[i] = newValue!;
-                      });
-                    },
-                    items: acompanhamientosDisponibles.map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                  SizedBox(height: 16.0),
-                ],
+            Text(
+              'Acompañamientos:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-            for (int i = 0; i < almuerzo.ensaladas; i++)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Ensalada ${i + 1}:',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  DropdownButton<String>(
-                    value: ensaladaSeleccionada,
-                    hint: Text('Seleccione una ensalada'),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        ensaladasSeleccionada[i] = newValue!;
-                      });
-                    },
-                    items: ensaladasDisponibles.map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                  SizedBox(height: 16.0),
-                ],
+            ),
+            SizedBox(height: 8),
+            DropdownButton<String?>(
+              value: selectedAcompanhamiento1,
+              onChanged: (newValue) {
+                setState(() {
+                  selectedAcompanhamiento1 = newValue;
+                });
+              },
+              items: acompanhamientos1Options.map((option) {
+                return DropdownMenuItem<String?>(
+                  value: option,
+                  child: Text(option ?? 'No seleccionado'),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 16),
+            DropdownButton<String?>(
+              value: selectedAcompanhamiento2,
+              onChanged: (newValue) {
+                setState(() {
+                  selectedAcompanhamiento2 = newValue;
+                });
+              },
+              items: acompanhamientos2Options.map((option) {
+                return DropdownMenuItem<String?>(
+                  value: option,
+                  child: Text(option ?? 'No seleccionado'),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 32),
+            Text(
+              'Ensaladas:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
+            ),
+            SizedBox(height: 8),
+            DropdownButton<String?>(
+              value: selectedEnsalada,
+              onChanged: (newValue) {
+                setState(() {
+                  selectedEnsalada = newValue;
+                });
+              },
+              items: ensaladasOptions.map((option) {
+                return DropdownMenuItem<String?>(
+                  value: option,
+                  child: Text(option ?? 'No seleccionada'),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 32),
+            Row(
+              children: [
+                Checkbox(
+                  value: isParaLlevar,
+                  onChanged: (newValue) {
+                    setState(() {
+                      isParaLlevar = newValue ?? false;
+                    });
+                  },
+                ),
+                Text(
+                  'Para llevar',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Acción a realizar al presionar el botón de finalizar
-          // Puedes utilizar las variables acompanhamientoSeleccionado y ensaladaSeleccionada
-        },
+        onPressed: () => _onFinalizeButtonPressed(),
         child: Icon(Icons.check),
+        backgroundColor: Color.fromARGB(255, 107, 142, 35),
       ),
+    );
+  }
+
+  void _onFinalizeButtonPressed() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Resumen del pedido'),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Almuerzo: ${widget.almuerzo.nombre}', style: TextStyle(fontSize: 18)),
+                SizedBox(height: 16),
+                Text('Acompañamientos:', style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(height: 8),
+                Text('- Acompañamiento 1: ${selectedAcompanhamiento1 ?? "No seleccionado"}', style: TextStyle(fontSize: 16)),
+                Text('- Acompañamiento 2: ${selectedAcompanhamiento2 ?? "No seleccionado"}', style: TextStyle(fontSize: 16)),
+                SizedBox(height: 16),
+                Text('Ensaladas:', style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(height: 8),
+                Text('- Ensalada: ${selectedEnsalada ?? "No seleccionada"}', style: TextStyle(fontSize: 16)),
+                SizedBox(height: 16),
+                Text('Pedido para llevar: ${isParaLlevar ? "Sí" : "No"}', style: TextStyle(fontSize: 16)),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cerrar', style: TextStyle(fontSize: 16)),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/screens/app_screens/menu_screen.dart
+++ lib/screens/app_screens/menu_screen.dart
@@ -22,7 +22,38 @@ class MenuScreen extends StatelessWidget {
       acompanhamientos: 1,
       ensaladas: 2,
     ),
-    // Agrega más objetos Almuerzo según tus necesidades
+    Almuerzo(
+      nombre: "Almuerzo 3",
+      descripcion: "Descripción del almuerzo 3",
+      precio: 12.49,
+      imagen: "assets/images/upb_logo.jpg",
+      acompanhamientos: 2,
+      ensaladas: 1,
+    ),
+    Almuerzo(
+      nombre: "Almuerzo 4",
+      descripcion: "Descripción del almuerzo 4",
+      precio: 8.99,
+      imagen: "assets/images/upb_logo.jpg",
+      acompanhamientos: 1,
+      ensaladas: 1,
+    ),
+    Almuerzo(
+      nombre: "Almuerzo 5",
+      descripcion: "Descripción del almuerzo 5",
+      precio: 11.99,
+      imagen: "assets/images/upb_logo.jpg",
+      acompanhamientos: 2,
+      ensaladas: 2,
+    ),
+    Almuerzo(
+      nombre: "Almuerzo 6",
+      descripcion: "Descripción del almuerzo 6",
+      precio: 9.49,
+      imagen: "assets/images/upb_logo.jpg",
+      acompanhamientos: 1,
+      ensaladas: 1,
+    ),
   ];
 
   @override
@@ -31,9 +62,20 @@ class MenuScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 107, 142, 35),
         centerTitle: true,
-        title: Text(
-          '¿Que vas a comer hoy?',
-          style: TextStyle(fontSize: 20.0),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.restaurant, size: 24, color: Colors.white),
+            SizedBox(width: 8),
+            Text(
+              '¿Qué vas a comer hoy?',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.2,
+              ),
+            ),
+          ],
         ),
         elevation: 0,
       ),
@@ -44,7 +86,7 @@ class MenuScreen extends StatelessWidget {
           Padding(
             padding: EdgeInsets.all(16.0),
             child: Text(
-              'Nuestro menu de hoy:',
+              'Nuestro menú de hoy:',
               style: TextStyle(
                 fontSize: 24.0,
                 fontWeight: FontWeight.bold,
@@ -62,25 +104,63 @@ class MenuScreen extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => AcompanhamientosScreen(almuerzo: almuerzo),
+                        builder: (context) => AcompanhamientosScreen(
+                          almuerzo: almuerzo,
+                          ensaladasSeleccionadas: [], // Lista vacía
+                          acompanhamientosSeleccionados: [], // Lista vacía
+                        ),
                       ),
                     );
                   },
                   child: Container(
                     margin: margin,
                     decoration: BoxDecoration(
-                      color: Colors.transparent,
+                      color: Colors.white,
                       borderRadius: BorderRadius.circular(8.0),
                       border: Border.all(
                         color: Colors.grey,
                         width: 1.0,
                       ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 2,
+                          blurRadius: 4,
+                          offset: Offset(0, 2), // changes position of shadow
+                        ),
+                      ],
                     ),
                     child: ListTile(
-                      leading: Image.asset(almuerzo.imagen),
-                      title: Text(almuerzo.nombre),
-                      subtitle: Text(almuerzo.descripcion),
-                      trailing: Text("Bs. ${almuerzo.precio.toStringAsFixed(2)}"),
+                      leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset(
+                          almuerzo.imagen,
+                          width: 60,
+                          height: 60,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      title: Text(
+                        almuerzo.nombre,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      subtitle: Text(
+                        almuerzo.descripcion,
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      trailing: Text(
+                        "Bs. ${almuerzo.precio.toStringAsFixed(2)}",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                          fontSize: 16,
+                        ),
+                      ),
                     ),
                   ),
                 );
